7f80bb831cdcd7dacaf82f5379d3a8bdd0c99199
Betanzos <josemanuel.betanzos@icloud.com>
2021-05-08 01:25:00 +0200

    Diseño del cartón finalizado

--- src/app/pages/carton/carton.component.ts
+++ src/app/pages/carton/carton.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { element } from 'protractor';
 
 @Component({
   selector: 'app-carton',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CartonComponent implements OnInit {
 
-  constructor() { }
+  public lotoNumbers: Array<Number>;
+  public lotoNumbersCalled: Array<Boolean>;
+
+  constructor() {
+    this.lotoNumbers = new Array<Number>();
+    this.lotoNumbersCalled = new Array<Boolean>();
+
+    for (let index = 0; index < 10; index++) {
+      let number = this.randomIntFromInterval(1,50);
+      while(this.lotoNumbers.includes(number)) {
+        number = this.randomIntFromInterval(1,50);
+      }
+      this.lotoNumbers.push(number) 
+      this.lotoNumbersCalled.push(false);     
+    }
+    this.lotoNumbers.sort(this.sortNumber);
+  }
 
   ngOnInit(): void {
   }
+
+  public callNumber(i: number) {
+    this.lotoNumbersCalled[i] = !this.lotoNumbersCalled[i];
+    console.log("call")
+  }
+
+  private randomIntFromInterval(min: number, max: number) { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  private sortNumber ( a, b ){ return a - b; }
 
 }
