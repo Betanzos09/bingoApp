8085d2e42bab0241356eec069a026868c03cd346
Betanzos <josemanuel.betanzos@icloud.com>
2021-05-08 02:41:45 +0200

    Componente Bombo Terminado

--- src/app/pages/bombo/bombo.component.ts
+++ src/app/pages/bombo/bombo.component.ts
@@ -7,11 +7,32 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BomboComponent implements OnInit {
 
-  constructor() { }
+  public numbers: Array<Number>;
+  public numbersCalled: Array<Boolean>;
+  public lastBall: Number = 0;
+
+
+  constructor() { 
+    this.numbers = Array.from({length: 50}, (_, i) => i + 1);
+    this.numbersCalled = new Array(50).fill(false);
+  }
 
   ngOnInit(): void {
   }
 
+  public nextBall() {
+    if(this.numbersCalled.includes(false)) {
+      let number = this.randomIntFromInterval(0,49);
+      while(this.numbersCalled[number] === true) {
+        number = this.randomIntFromInterval(0,49);
+      }
+      this.numbersCalled[number] = true;
+      this.lastBall = number +1;
+    }
+  }
   
+  private randomIntFromInterval(min: number, max: number) { 
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
 
 }
