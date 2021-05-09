bccef28f328189380f5e76d0a0a669a642d4087f
Betanzos <josemanuel.betanzos@icloud.com>
2021-05-09 12:06:09 +0200

    Estilos aún mejor

--- src/app/pages/bombo/bombo.component.ts
+++ src/app/pages/bombo/bombo.component.ts
@@ -1,16 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { element } from 'protractor';
+import { Subscription, fromEvent } from 'rxjs';
 
 @Component({
   selector: 'app-bombo',
   templateUrl: './bombo.component.html',
   styleUrls: ['./bombo.component.scss']
 })
-export class BomboComponent implements OnInit {
+export class BomboComponent implements OnInit, OnDestroy {
 
   public numbers: Array<Number>;
   public numbersCalled: Array<Boolean>;
   public lastBall: Number = 0;
   public shake: boolean = true;
+  public subscriptor: Subscription;
 
 
   constructor() { 
@@ -19,6 +22,22 @@ export class BomboComponent implements OnInit {
   }
 
   ngOnInit(): void {
+    this.subscriptor =  fromEvent(window, 'resize')
+        .subscribe(res => {
+          let numbersClass = document.querySelector<HTMLElement>(".bombo-screen-content-numbers");
+          let heightNumber = numbersClass.offsetHeight;
+          let widthNumber = numbersClass.offsetWidth;
+          if(widthNumber < 890) {
+            this.resize();
+          }
+        });
+    setTimeout(()=>{
+      this.resize();
+    }, 100);
+  }
+
+  ngOnDestroy():void {
+    this.subscriptor.unsubscribe();
   }
 
   public nextBall() {
@@ -32,9 +51,31 @@ export class BomboComponent implements OnInit {
     }
     this.shake = false;
   }
-  
+
   private randomIntFromInterval(min: number, max: number) { 
     return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  private resize() {
+    let numbersClass = document.querySelector<HTMLElement>(".bombo-screen-content-numbers");
+    let heightNumber = numbersClass.offsetHeight;
+    let widthNumber = numbersClass.offsetWidth;
+    let value = Math.trunc(Math.sqrt((heightNumber * widthNumber)/50)) - 12;
+    let numberClass = document.getElementsByClassName("bombo-screen-content-numbers-number");
+    for (let index = 0; index < numberClass.length; index++) {
+      let element = numberClass[index] as HTMLElement;
+      element.style.height = value + "px";
+      element.style.width = value + "px";
+      element.style.opacity = "1";
+    }
+    let numberFontClass = document.getElementsByClassName("bombo-screen-content-numbers-number-text");
+    for (let index = 0; index < numberFontClass.length; index++) {
+      let element = numberFontClass[index] as HTMLElement;
+      element.style.fontSize = value/2 + "px";
+      element.style.height = value/1.5 + "px";
+      element.style.width = value/1.5 + "px";
+
+    }
   }
 
 }
